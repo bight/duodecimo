5e2117f5873fd16d8456203bc14be747099be5c3
Ned Zimmerman <ned@bight.dev>
2026-09-05 22:14:57 -0300

    ci: add lint workflow (#19)

--- sugarcube.config.js
+++ sugarcube.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from "@sugarcube-sh/cli";
 
 export default defineConfig({
-    content: [
-        "resources/views/**/*.blade.php"
-    ],
-    components: "resources/css/components",
-    cube: "resources/css/cube",
-    variables: {
-        permutations: [
-            { input: { mode: "light" }, selector: ":root" },
-            {
-                input: { mode: "dark" },
-                selector: ":root",
-                atRule: "@media (prefers-color-scheme: dark)"
-            }
-        ]
-    }
+	content: ["resources/views/**/*.blade.php"],
+	components: "resources/css/components",
+	cube: "resources/css/cube",
+	variables: {
+		permutations: [
+			{ input: { mode: "light" }, selector: ":root" },
+			{
+				input: { mode: "dark" },
+				selector: ":root",
+				atRule: "@media (prefers-color-scheme: dark)",
+			},
+		],
+	},
 });
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import sugarcube from "@sugarcube-sh/vite";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            fonts: [
-                bunny('Atkinson Hyperlegible Next', {
-                    weights: [400, 500, 600],
-                }),
-                bunny('Source Serif 4', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        sugarcube(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+	plugins: [
+		laravel({
+			input: ["resources/css/app.css", "resources/js/app.js"],
+			refresh: true,
+			fonts: [
+				bunny("Atkinson Hyperlegible Next", {
+					weights: [400, 500, 600],
+				}),
+				bunny("Source Serif 4", {
+					weights: [400, 500, 600],
+				}),
+			],
+		}),
+		sugarcube(),
+	],
+	server: {
+		watch: {
+			ignored: ["**/storage/framework/views/**"],
+		},
+	},
 });
